194ad6ef6f32892019600f21ac924a8fa558c9a3
Glauber Marques <glauberfmarques@gmail.com>
2023-05-30 20:03:01 -0600

    Keep urls created by a user accessible just for that custonmer in dashboard of the application

--- express_server.js
+++ express_server.js
@@ -11,12 +11,10 @@ app.use(cookieParser());
 // Database of shortened URLs
 const urlDatabase = {
   b6UTxQ: {
-    shortURL: "b6UTxQ",
     longURL: "https://www.tsn.ca",
     userID: "aJ48lW",
   },
   i3BoGr: {
-    shortURL: "i3BoGr",
     longURL: "https://www.google.ca",
     userID: "aJ48lW",
   },
@@ -49,6 +47,13 @@ const generateRandomString = (length) => {
   return randomString;
 };
 
+// Helper function to retrieve URLs for a specific user
+const urlsForUser = (userId) => {
+  return Object.fromEntries(
+    Object.entries(urlDatabase).filter(([_, url]) => url.userID === userId)
+  );
+};
+
 // Route to create a new shortened URL
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies.user_id];
@@ -65,9 +70,19 @@ app.get("/urls/:id", (req, res) => {
   const { id } = req.params;
   const user = users[req.cookies.user_id];
   const url = urlDatabase[id];
+  // Check if the user is logged in
+  if (!user) {
+    res.status(401).send("You must be logged in to access this page.");
+    return;
+  }
   // Check if the URL exists in the database
   if (!url) {
     res.status(404).send("URL Not Found");
+    return;
+  }
+  // Check if the URL belongs to the logged-in user
+  if (url.userID !== user.id) {
+    res.status(403).send("Access Denied");
     return;
   }
 
@@ -89,6 +104,16 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+// GET route for registering a new user
+app.get('/register', (req, res) => {
+  res.render('register', { user: req.user });
+});
+
+// GET route for login In
+app.get('/login', (req, res) => {
+  res.render('login', { user: req.user });
+});
+
 // Default route
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -99,166 +124,160 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// Route to display the list of URLs for a user
+// Route to display the list of URLs
 app.get("/urls", (req, res) => {
   const user = users[req.cookies.user_id];
-
   // Check if the user is logged in
   if (!user) {
-    res.redirect("/login");
+    res.render("urls_login_prompt", { user });
     return;
   }
-
-  const userUrls = Object.fromEntries(
-    Object.entries(urlDatabase).filter(([key, value]) => value.userID === user.id)
-  );
-
-  const templateVars = {
-    user: user,
-    urls: userUrls
-  };
-
+  // Retrieve the URLs belonging to the logged-in user
+  const userUrls = urlsForUser(user.id);
+  const templateVars = { urls: userUrls, user };
   res.render("urls_index", templateVars);
 });
 
-// Route to register a new user
-app.get("/register", (req, res) => {
-  const user = users[req.cookies.user_id];
-  // Check if the user is already logged in
-  if (user) {
-    res.redirect("/urls");
-  } else {
-    res.render("register", { user });
-  }
-});
+// Route to handle user registration
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
 
-// Route to login
-app.get("/login", (req, res) => {
-  const user = users[req.cookies.user_id];
-  // Check if the user is already logged in
-  if (user) {
-    res.redirect("/urls");
-  } else {
-    res.render("login", { user });
-  }
-});
-
-// Route to test rendering HTML with EJS
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-// Route to create a new shortened URL
-app.post("/urls", (req, res) => {
-  const user = users[req.cookies.user_id];
-  // Check if the user is logged in
-  if (!user) {
-    res.status(401).send("You must be logged in to shorten URLs.");
+  if (!email || !password) {
+    res.status(400).send("Email and password fields cannot be empty.");
     return;
   }
+
+  const existingUser = Object.values(users).find(user => user.email === email);
+
+  if (existingUser) {
+    res.status(400).send("Email is already registered.");
+    return;
+  }
+
+  const userId = generateRandomString(6);
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  users[userId] = { id: userId, email: email, password: hashedPassword };
+
+  res.cookie("user_id", userId);
+  res.redirect("/urls");
+});
+
+// Route to handle user login
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = Object.values(users).find(user => user.email === email);
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    res.status(403).send("Invalid email or password.");
+    return;
+  }
+
+  res.cookie("user_id", user.id);
+  res.redirect("/urls");
+});
+
+// Route to handle user logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
+
+// Route to handle URL creation
+app.post("/urls", (req, res) => {
+  const user = users[req.cookies.user_id];
+
+  if (!user) {
+    res.status(401).send("You must be logged in to create a URL.");
+    return;
+  }
+
   const shortURL = generateRandomString(6);
   const longURL = req.body.longURL;
 
   urlDatabase[shortURL] = {
     longURL: longURL,
-    userID: user.id
+    userID: user.id,
   };
 
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Route to update the long URL of a shortened URL
-app.post('/urls/:id', (req, res) => {
-  const id = req.params.id;
-  const newLongURL = req.body.longURL;
+// Route to handle URL update
+app.post("/urls/:id/update", (req, res) => {
+  const { id } = req.params;
+  const user = users[req.cookies.user_id];
+  const url = urlDatabase[id];
 
-  urlDatabase[id].longURL = newLongURL;
+  if (!user) {
+    res.status(401).send("You must be logged in to update a URL.");
+    return;
+  }
 
-  res.redirect('/urls');
+  if (!url) {
+    res.status(404).send("URL Not Found");
+    return;
+  }
+
+  if (url.userID !== user.id) {
+    res.status(403).send("Access Denied");
+    return;
+  }
+
+  urlDatabase[id].longURL = req.body.longURL;
+  res.redirect("/urls");
 });
 
-// Route to delete a shortened URL
+// Route to handle URL deletion
 app.post("/urls/:id/delete", (req, res) => {
   const { id } = req.params;
+  const user = users[req.cookies.user_id];
+  const url = urlDatabase[id];
+
+  if (!user) {
+    res.status(401).send("You must be logged in to delete a URL.");
+    return;
+  }
+
+  if (!url) {
+    res.status(404).send("URL Not Found");
+    return;
+  }
+
+  if (url.userID !== user.id) {
+    res.status(403).send("Access Denied");
+    return;
+  }
+
   delete urlDatabase[id];
   res.redirect("/urls");
 });
 
-// Route to handle the login form submission
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
+// Route to handle URL update
+app.post("/urls/:id/update", (req, res) => {
+  const { id } = req.params;
+  const user = users[req.cookies.user_id];
+  const url = urlDatabase[id];
 
-  // Look up the user by email
-  const user = getUserByEmail(email);
-
-  // Check if user with that email exists
   if (!user) {
-    res.status(403).send("Invalid email");
+    res.status(401).send("You must be logged in to update a URL.");
     return;
   }
 
-  // Check if the password matches
-  if (!bcrypt.compareSync(password, user.password)) {
-    res.status(403).send("Invalid password");
+  if (!url) {
+    res.status(404).send("URL Not Found");
     return;
   }
 
-  // Set the user_id cookie with the matching user's ID
-  res.cookie("user_id", user.id);
+  if (url.userID !== user.id) {
+    res.status(403).send("Access Denied");
+    return;
+  }
 
-  // Redirect to /urls
+  urlDatabase[id].longURL = req.body.longURL;
   res.redirect("/urls");
 });
-
-// Route to handle the logout action
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id"); // Clear the user_id cookie
-  res.redirect("/login");
-});
-
-// Route to register a new user
-app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-
-  // Check if email or password are empty strings
-  if (email === "" || password === "") {
-    res.status(400).send("Email and password cannot be empty");
-    return;
-  }
-
-  // Check if email already exists in the users object
-  const user = getUserByEmail(email);
-  if (user) {
-    res.status(400).send("Email already registered");
-    return;
-  }
-
-  const userId = generateRandomString();
-
-  users[userId] = {
-    id: userId,
-    email,
-    password: bcrypt.hashSync(password, 10),
-  };
-
-  // Set a user_id cookie containing the user's newly generated ID
-  res.cookie("user_id", userId);
-
-  // Redirect the user to the /urls page
-  res.redirect("/urls");
-});
-
-// Helper function to lookup a user by email
-const getUserByEmail = (email) => {
-  for (const userId in users) {
-    if (users[userId].email === email) {
-      return users[userId];
-    }
-  }
-  return null;
-};
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Server listening on port ${PORT}`);
 });
